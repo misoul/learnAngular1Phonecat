0a7f8742495b024f9c4bd3321b5124be9426c38d
Vinh To <misoul@gmail.com>
2016-07-20 10:27:27 -0700

    Create a standalone nodejs REST server (to serve in place of http-server).

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 'use strict';
 
 var express = require('express'),
-    wines = require('./routes/employeeInMemory');
+    employees = require('./routes/employeeInMemory'),
+    phones = require('./routes/phonesInMemory');
 
 var app = express();
 
-app.get('/employees/:id/reports', wines.findByManager);
-app.get('/employees/:id', wines.findById);
-app.get('/employees', wines.findAll);
+app.get('/employees/:id/reports', employees.findByManager);
+app.get('/employees/:id', employees.findById);
+app.get('/employees', employees.findAll);
+
+app.get('/phones/phones.json', phones.findAll);
+app.get('/phones/:phoneId', phones.get);
+app.use('/img/phones', express.static(__dirname + '/app/img/phones'));
+app.use('/app', express.static(__dirname + '/app'));
+
 
 app.listen(3000);
-console.log('Listening on port 3000...');
+console.log((new Date()).getMinutes() + ': listening on port 3000.');
